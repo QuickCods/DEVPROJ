b128e070a236b5e814ba2f1dcdbdf2f543b7b557
Beatriz Freitas <beatriz.freitas.236412159@msft.cesae.pt>
2025-07-07 17:59:49 +0100

    Comunicaçao back e front via http organizada

--- src/FreyrFund.Api/Program.cs
+++ src/FreyrFund.Api/Program.cs
@@ -37,37 +37,9 @@ app.UseSwaggerUI(c =>
 app.UseHttpsRedirection();
 app.UseCors("AllowAngularDev");
 
-// 5) Seu endpoint Test
-app.MapGet("/Test", () => Results.Ok(new { message = "API funciona!" }))
-   .WithName("Test")
-   .WithOpenApi();
+app.MapControllers();    
 
-// 6) WeatherForecast (exemplo existente)
-app.MapGet("/weatherforecast", () =>
-{
-    var summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild",
-        "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast(
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        )
-    ).ToArray();
-
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
 
 app.Run();
 
-// Modelo do forecast
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
